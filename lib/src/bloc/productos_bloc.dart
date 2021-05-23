fb75b59cc3e15a05fe7242a9c34941f1e4ff39dd
Daniel Ibarra <31217761+Daniel100110@users.noreply.github.com>
2021-05-23 11:38:00 -0700

    first commit

--- lib/src/bloc/productos_bloc.dart
+++ lib/src/bloc/productos_bloc.dart
@@ -1,9 +1,12 @@
 import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/providers/productos_provider.dart';
 import 'package:rxdart/rxdart.dart';
 
 class ProductosBloc {
   final _productosController = new BehaviorSubject<List<ProductoModel>>();
   final _cargandoController = new BehaviorSubject<bool>();
+
+  final _productosProvider = new ProductosProvider();
 
   dispose() {
     _productosController?.close();
